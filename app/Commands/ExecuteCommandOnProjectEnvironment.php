220daa8bd386e56f2f393903cf55682d4b37513d
Bryan Gruneberg <hello@bryangruneberg.com>
2025-03-22 18:12:49 -0600

    Adds the ssh ability

---
+++ app/Commands/ExecuteCommandOnProjectEnvironment.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class ExecuteCommandOnProjectEnvironment extends LagoonCommandBase
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'execute-command-on-project-environment {--i|identity_file=~/.ssh/id_rsa} {--p|project=} {--e|environment=} {--s|service=cli} {--c|container=cli} {execute}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Execute a command on a project environment';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $identity_file = $this->option("identity_file");
+        $this->initLagoonClient($identity_file);
+
+        $project = $this->option('project');
+        if (empty($project)) {
+            $this->error('Project is required');
+            return 1;
+        }
+
+        $environment = $this->option('environment');
+        if (empty($environment)) {
+            $this->error('Environment is required');
+            return 1;
+        }
+
+
+        $execute = $this->argument('execute');
+        if (empty($execute)) {
+            $this->error('Command is required');
+            return 1;
+        }
+
+        $service = $this->option('service');
+        $container = $this->option('container');
+
+        $this->info("Executing command in project [" . $project . "] on environment [" . $environment . "]");
+        $result = $this->LagoonClient->executeCommandOnProjectEnvironment($project, $environment, $execute, $service, $container);
+
+        if(isset($result['command'])) {
+            $this->warn("Command: " . $result['command']);
+        }
+
+        if(isset($result['result'])) {
+            $this->warn("Result: " . $result['result']);
+        }
+
+        if(isset($result['result_text'])) {
+            $this->warn("Result Text: " . $result['result_text']);
+        }
+
+        if(isset($result['output'])) {
+            $this->info("Output: \n" . $result['output']);
+        }
+
+        if(isset($result['error'])) {
+            $this->error("Error: \n" . $result['error']);
+        }
+    }
+}
